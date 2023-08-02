b1d60834bef86d49cabd52bc7eac908b7b6bee0c
HsiaoHungWang <wang@ispan.com.tw>
2023-08-02 14:55:56 +0800

    讀取所有產品資料

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,15 +1,38 @@
 <template>
       <h2>Product List</h2>  
+      <table class="table table-bordered">
+        <thead> <tr><th>產品編號 </th> <th>產品名稱 </th><th>產品</th>                        <th>製造</th></tr></thead>
+        <tbody>
+            <tr v-for="{ id, name, price, make } in products" :key="id">
+                <td>{{ id }}</td>
+                <td>{{ name }}</td>
+                <td>{{ price }}</td>
+                <td>{{ make }}</td>
+            </tr>
+        </tbody>
+    </table>
+
 </template>
     
 <script setup>
+import {ref, reactive} from 'vue'
     import axios from 'axios'
- const loadData = async()=>{
-   const URL = `${import.meta.env.VITE_API_JAVAURL}products/1`
-   const response = await axios.get(URL)
-   console.log(response.data) 
+    const products = ref([])
+    const datas = reactive({
+        "start" :0,
+        "rows" : 0,
+        "name":"",
+        "sortOrder":"asc",
+        "sortType":"id"
+     })
+    const URL = import.meta.env.VITE_API_JAVAURL
+ const loadProducts = async()=>{
+   const URLAPI = `${URL}products/find`
+   const response = await axios.post(URLAPI,datas)
+   products.value = response.data.list
+   console.log(products.value)
 }
- loadData()
+loadProducts()
 
 </script>
     
