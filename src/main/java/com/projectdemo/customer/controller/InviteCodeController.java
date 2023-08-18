acfcd74acfb468af7e6a0a42ae59b3c313535859
DESKTOP-RAE7E6C\User <royliu351224@gmail.com>
2023-08-18 17:54:29 +0800

    更新了使用者邀請碼的新建方式

--- src/main/java/com/projectdemo/customer/controller/InviteCodeController.java
+++ src/main/java/com/projectdemo/customer/controller/InviteCodeController.java
@@ -1,12 +1,12 @@
 package com.projectdemo.customer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projectdemo.customer.bean.UserInviteCodeBean;
@@ -14,14 +14,14 @@ import com.projectdemo.customer.service.InviteCodeService;
 
 
 @RestController
+@CrossOrigin()
 public class InviteCodeController {
 	@Autowired
 	private InviteCodeService inviteCodeService;
 
 	@PostMapping("/inviteCode/create/{id}")
-	public UserInviteCodeBean inviteCodeCreate(@RequestBody UserInviteCodeBean userInviteCode,
-			@PathVariable Integer id) {
-		return inviteCodeService.InviteCodeCreate(userInviteCode, id);
+	public UserInviteCodeBean inviteCodeCreate(@PathVariable Integer id) {
+		return inviteCodeService.InviteCodeCreate(id);
 	}
 
 	@GetMapping("/inviteCode/read/{id}")
