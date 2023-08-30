2296265c2eba0b7de8b8e97e7ae4d5b5275c39e8
DESKTOP-RAE7E6C\User <gifjpgbig@gmail.com>
2023-08-30 16:35:17 +0800

    2023/8/30

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,7 +4,7 @@
       <a class="navbar-brand d-flex align-items-center" href="#"
         ><span
           class="bs-icon-sm bs-icon-rounded bs-icon-primary d-flex justify-content-center align-items-center me-2 bs-icon"
-          > 
+        >
           <!-- <svg
             class="bi bi-bezier"
             xmlns="http://www.w3.org/2000/svg"
@@ -20,9 +20,8 @@
             <path
               d="M6 4.5H1.866a1 1 0 1 0 0 1h2.668A6.517 6.517 0 0 0 1.814 9H2.5c.123 0 .244.015.358.043a5.517 5.517 0 0 1 3.185-3.185A1.503 1.503 0 0 1 6 5.5v-1zm3.957 1.358A1.5 1.5 0 0 0 10 5.5v-1h4.134a1 1 0 1 1 0 1h-2.668a6.517 6.517 0 0 1 2.72 3.5H13.5c-.123 0-.243.015-.358.043a5.517 5.517 0 0 0-3.185-3.185z"
             ></path></svg> -->
-            <i class="bi bi-headset"></i>
-            </span
-        >
+          <i class="bi bi-headset"></i>
+        </span>
         <span>客服人員系統</span></a
       ><button
         class="navbar-toggler"
@@ -32,31 +31,46 @@
         <span class="visually-hidden">Toggle navigation</span
         ><span class="navbar-toggler-icon"></span>
       </button>
-      <div id="navcol-5" class="collapse navbar-collapse d-flex align-items-center">
+      <div
+        id="navcol-5"
+        class="collapse navbar-collapse d-flex align-items-center"
+      >
         <ul class="navbar-nav ms-auto">
           <!-- <li class="nav-item">
             <a class="nav-link active" href="#">First Item</a>
           </li>
           <li class="nav-item"><a class="nav-link" href="#">Second Item</a></li>
           <li class="nav-item"><a class="nav-link" href="#">Third Item</a></li> -->
-          <li class="nav-item d-flex align-items-center justify-content-center m-2" style="color: white; text-align: center;">阿凡達, 早上好!</li>
+          <li
+            class="nav-item d-flex align-items-center justify-content-center m-2"
+            style="color: white; text-align: center"
+          >
+            阿凡達, 早上好!
+          </li>
           <li>
-            <img class="avatar" src="../img/avatar.jpg" alt="User Avatar">
+            <button
+              class="btn  dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton1"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+            <img class="avatar" src="../img/avatar.jpg" alt="User Avatar" />
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1"
+            style="position: absolute; top: 100%; right: 0; left: auto;">
+              <li><a class="dropdown-item" href="#">修改大頭貼</a></li>
+              <li><a class="dropdown-item" href="#">登出</a></li>
+            </ul>
           </li>
         </ul>
         <!-- <a class="btn btn-primary ms-md-2" role="button" href="#">Button</a> -->
-        
-
       </div>
     </div>
   </nav>
-
-
-
 </template>
 
-<script setup>
-</script>
+<script setup></script>
 
 <style scoped>
 .navbar {
@@ -71,5 +85,14 @@
   width: 50px;
   height: 50px;
   border-radius: 50%;
+}
+
+.dropdown-content {
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+  right: 0; /* 將下拉選單靠右對齊 */
 }
 </style>
