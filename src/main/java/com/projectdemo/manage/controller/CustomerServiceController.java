d18b7a0f5eba0aea2727a00b9205effa062c9eb1
DESKTOP-I1BKH0E\gifjp <gifjpgbig@gmail.com>
2023-08-31 09:50:36 +0800

    2023/8/31

--- src/main/java/com/projectdemo/manage/controller/CustomerServiceController.java
+++ src/main/java/com/projectdemo/manage/controller/CustomerServiceController.java
@@ -1,10 +1,12 @@
 package com.projectdemo.manage.controller;
 
 import java.io.IOException;
+import java.util.Base64;
 
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +17,7 @@ import org.springframework.web.multipart.MultipartFile;
 import com.projectdemo.manage.bean.CustomerServiceBean;
 import com.projectdemo.manage.service.CustomerServiceService;
 import com.projectdemo.shop.bean.ShopBean;
+import com.projectdemo.shop.service.ShopService;
 
 @RestController
 @CrossOrigin()
@@ -26,10 +29,8 @@ public class CustomerServiceController {
 	@PostMapping("/manage/cs/add")
 	public String add(@RequestPart String json, @RequestPart("photoFile") MultipartFile photoFile) {
 		JSONObject responseJson = new JSONObject();
-		
-		
+
 		JSONObject data = new JSONObject(json);
-		
 
 		try {
 			byte[] photo = photoFile.getBytes();
@@ -39,12 +40,12 @@ public class CustomerServiceController {
 			csb.setName(data.getString("name"));
 			csb.setPassword(data.getString("password"));
 			csb.setPhoto(photo);
-		
+
 			CustomerServiceBean addCDB = csService.addCDB(csb);
-			if(addCDB != null) {
+			if (addCDB != null) {
 				responseJson.put("message", "管理員新增成功！");
 				responseJson.put("success", true);
-			}else {
+			} else {
 				responseJson.put("message", "管理員新增失敗！");
 				responseJson.put("success", false);
 			}
@@ -52,38 +53,67 @@ public class CustomerServiceController {
 			responseJson.put("message", "相片規格不符合");
 			responseJson.put("success", false);
 			e.printStackTrace();
-		} 
-		
+		}
+
 		return responseJson.toString();
 	}
-	
-	@PostMapping("/addPhoto/{id}")
+
+	@PostMapping("/manage/cs/addPhoto/{id}")
 	public String addShopPhoto(@PathVariable("id") Integer id, @RequestBody MultipartFile photoFile) {
 		JSONObject json = new JSONObject();
-		ShopBean shopBean = shopService.findById(id);
+		CustomerServiceBean csb = new CustomerServiceBean();
 		try {
 			byte[] photo = photoFile.getBytes();
-			shopBean.setPhoto(photo);
-			ShopBean update = shopService.update(id, shopBean);
+			csb.setPhoto(photo);
+			CustomerServiceBean update = csService.update(id, csb);
 			if (update != null) {
 				json.put("success", true);
+				json.put("message", "照片更新成功");
 			} else {
 				json.put("success", false);
+				json.put("message", "照片更新失敗");
 			}
 		} catch (IOException e) {
 			json.put("message", "invalid photo");
+			json.put("success", false);
 			e.printStackTrace();
 		}
 		return json.toString();
 	}
+
+	/**
+	 * GET PHOTO BY CUSSERVICE ID
+	 * 
+	 * @param id cusService ID
+	 * @return Base64 photo
+	 */
 	
-	
+	@GetMapping("/manage/cs/findPhoto/{id}")
+	public String getPhoto(@PathVariable Integer id) {
+		JSONObject json = new JSONObject();
+		CustomerServiceBean csb = csService.findById(id);
+		if (csb == null) {
+			return null;
+		}
+		
+		byte[] photo = csb.getPhoto();
+		if (photo != null) {
+			String base64Image = Base64.getEncoder().encodeToString(photo);
+			json.put("success", true);
+			json.put("photo", base64Image);
+		} else {
+			json.put("success", false);
+		}
+		
+		return json.toString();
+	}
 
 	@PostMapping("/manage/cs/login")
 	public String login(@RequestBody String json) {
 		JSONObject responseJson = new JSONObject();
 		CustomerServiceBean cs = csService.checkLogin(json);
 		if (cs != null) {
+			responseJson.put("csID", cs.getId());
 			responseJson.put("csName", cs.getName());
 			responseJson.put("auth", cs.getAuthorizations());
 			responseJson.put("message", "登入成功！");
--- src/main/java/com/projectdemo/manage/service/CustomerServiceService.java
+++ src/main/java/com/projectdemo/manage/service/CustomerServiceService.java
@@ -1,5 +1,7 @@
 package com.projectdemo.manage.service;
 
+import java.util.Optional;
+
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import com.projectdemo.manage.bean.CustomerServiceBean;
 import com.projectdemo.manage.dao.CustomerServiceRepository;
 import com.projectdemo.order.bean.DeliverDetailBean;
+import com.projectdemo.shop.bean.ShopBean;
 
 @Configuration
 public class CustomerServiceService {
@@ -18,6 +21,15 @@ public class CustomerServiceService {
 		return csRepo.save(csb);
 	}
 	
+	public CustomerServiceBean update(Integer id, CustomerServiceBean csb) {
+		Optional<CustomerServiceBean> optional = csRepo.findById(id);
+		if(optional.isPresent()) {
+			CustomerServiceBean csbb = optional.get();
+			csbb.setPhoto(csb.getPhoto());
+			return csRepo.save(csbb);
+		}
+		return null;
+	}
 	
 	public CustomerServiceBean checkLogin(String json) {
 		JSONObject data = new JSONObject(json);
@@ -31,6 +43,13 @@ public class CustomerServiceService {
 		return null;
 	}
 	
+	public CustomerServiceBean findById(Integer id) {
+		Optional<CustomerServiceBean> optional = csRepo.findById(id);
+		if(optional.isPresent()) {
+			return optional.get();
+		}
+		return null;
+	}
 	
 	
 	
